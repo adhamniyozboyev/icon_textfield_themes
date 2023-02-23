675b944c9564cfadece91d8f57cbae279119f988
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-02-23 22:42:37 +0500

    make a login page

--- lib/main.dart
+++ lib/main.dart
@@ -14,17 +14,148 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Widget func(IconData icon,[Color color=Colors.black]) {
-    return Icon(
-      icon,
-      size: 70,
-    );
-  }
-
+  TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body:TextField()
-    );
+        backgroundColor: Color.fromARGB(255, 197, 224, 246),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'SIGN IN',
+                style: TextStyle(fontSize: 40, color: Colors.white),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Row(
+                children: [
+                  Text(
+                    '        Username',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.blueGrey,
+                height: 60,
+                width: 350,
+                child: TextField(
+                  decoration: InputDecoration(
+                      prefixIconColor: Colors.white,
+                      prefixIcon: Icon(Icons.person_pin),
+                      hintText: 'Enter your username',
+                      hintStyle: TextStyle(color: Colors.white),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(0))),
+                  textCapitalization: TextCapitalization.words,
+                  // controller: controller,
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Row(
+                children: [
+                  Text(
+                    '        Password',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.blueGrey,
+                height: 60,
+                width: 350,
+                child: TextField(
+                  decoration: InputDecoration(
+                      prefixIconColor: Colors.white,
+                      prefixIcon: Icon(Icons.lock),
+                      hintText: 'Enter your password',
+                      hintStyle: TextStyle(color: Colors.white),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(0))),
+                  textCapitalization: TextCapitalization.words,
+                  // controller: controller,
+                ),
+              ),
+              Text(
+                '                                                                 Forget Passwrod?',
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(
+                height: 38,
+              ),
+              Container(
+                child: Center(
+                  child: Text(
+                    'LOGIN',
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ),
+                height: 60,
+                width: 280,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(50))),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  '       -OR-\n\n Sign in with',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 60,
+                    width: 60,
+                    child: Center(
+                      child: Text(
+                        'f',
+                        style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 50,
+                  ),
+                  Container(
+                    height: 60,
+                    width: 60,
+                    child: Center(
+                      child: Text(
+                        'G',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 114, 237, 14),
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+              
+            ],
+          ),
+        ));
   }
 }
