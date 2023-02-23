a0d28ba324f5840a80c8f9a5b125146bd3554c39
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-02-23 14:48:25 +0500

    icons finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-
-void main(){
-  runApp(MaterialApp(home: MyApp(),));
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatefulWidget {
@@ -13,8 +14,38 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  Widget func(IconData icon,[Color color=Colors.black]) {
+    return Icon(
+      icon,
+      size: 70,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body:
+            Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          func(Icons.home,),
+          func(Icons.house),
+          func(Icons.home_work),
+          func(Icons.house_sharp),
+          func(Icons.add_home_work),
+        ],
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          func(Icons.pool,Colors.blue),
+          func(Icons.water,Colors.blue),
+          func(Icons.water_sharp,Colors.blue),
+          func(Icons.pool_rounded,Colors.blue),
+          func(Icons.pool_sharp,Colors.blue),
+        ],
+      ),
+    ]));
   }
 }
