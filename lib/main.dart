a8d673cc0cc5a3fa60c5710d11034bcbbf5fb081
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-02-23 22:52:37 +0500

    finish a design

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: MyApp(),
   ));
 }
@@ -18,7 +19,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromARGB(255, 197, 224, 246),
+        backgroundColor: Color.fromARGB(255, 73, 128, 38),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -39,7 +40,11 @@ class _MyAppState extends State<MyApp> {
                 ],
               ),
               Container(
-                color: Colors.blueGrey,
+                decoration: BoxDecoration(
+                    color: Colors.blueGrey,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    )),
                 height: 60,
                 width: 350,
                 child: TextField(
@@ -49,7 +54,7 @@ class _MyAppState extends State<MyApp> {
                       hintText: 'Enter your username',
                       hintStyle: TextStyle(color: Colors.white),
                       border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(0))),
+                          borderRadius: BorderRadius.circular(20))),
                   textCapitalization: TextCapitalization.words,
                   // controller: controller,
                 ),
@@ -66,7 +71,11 @@ class _MyAppState extends State<MyApp> {
                 ],
               ),
               Container(
-                color: Colors.blueGrey,
+                decoration: BoxDecoration(
+                    color: Colors.blueGrey,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    )),
                 height: 60,
                 width: 350,
                 child: TextField(
@@ -76,7 +85,7 @@ class _MyAppState extends State<MyApp> {
                       hintText: 'Enter your password',
                       hintStyle: TextStyle(color: Colors.white),
                       border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(0))),
+                          borderRadius: BorderRadius.circular(20))),
                   textCapitalization: TextCapitalization.words,
                   // controller: controller,
                 ),
@@ -102,7 +111,7 @@ class _MyAppState extends State<MyApp> {
                     borderRadius: BorderRadius.all(Radius.circular(50))),
               ),
               Padding(
-                padding: const EdgeInsets.all(20.0),
+                padding: const EdgeInsets.all(15.0),
                 child: Text(
                   '       -OR-\n\n Sign in with',
                   style: TextStyle(
@@ -111,49 +120,55 @@ class _MyAppState extends State<MyApp> {
                   ),
                 ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 60,
-                    width: 60,
-                    child: Center(
-                      child: Text(
-                        'f',
-                        style: TextStyle(
-                            color: Colors.blue,
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold),
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 60,
+                      width: 60,
+                      child: Center(
+                        child: Text(
+                          'f',
+                          style: TextStyle(
+                              color: Colors.blue,
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
                       ),
                     ),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.white,
+                    SizedBox(
+                      width: 50,
                     ),
-                  ),
-                  SizedBox(
-                    width: 50,
-                  ),
-                  Container(
-                    height: 60,
-                    width: 60,
-                    child: Center(
-                      child: Text(
-                        'G',
-                        style: TextStyle(
-                            color: Color.fromARGB(255, 114, 237, 14),
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold),
+                    Container(
+                      height: 60,
+                      width: 60,
+                      child: Center(
+                        child: Text(
+                          'G',
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 114, 237, 14),
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold),
+                        ),
                       ),
-                    ),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.white,
-                    ),
-                  )
-                ],
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
               ),
-              
+              Text(
+                'Don\'t have an Account? Sign Up ',
+                style: TextStyle(color: Colors.white),
+              )
             ],
           ),
         ));
