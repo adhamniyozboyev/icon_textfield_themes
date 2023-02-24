53ebd15f4f8ed917b8bd545d4a6865f5d78d8762
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-02-24 09:31:01 +0500

    visible off password

--- lib/main.dart
+++ lib/main.dart
@@ -14,31 +14,41 @@ class MyWidget extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<MyWidget> {
+  bool b = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           TextField(
-
             keyboardType: TextInputType.name,
             decoration: InputDecoration(
                 hintText: 'Enter your name',
                 helperText: 'name',
-                prefix: Icon(Icons.percent),
+                prefix: Icon(Icons.people),
                 suffix: Icon(Icons.add_circle_outline_sharp),
                 border: OutlineInputBorder()),
           ),
-             TextField(  keyboardType: TextInputType.name,
+          TextField(
+            obscureText: b,
+            keyboardType: TextInputType.name,
             decoration: InputDecoration(
-                hintText: 'Enter your name',
-                helperText: 'name',
-                prefix: Icon(Icons.percent),
-                suffix: Icon(Icons.add_circle_outline_sharp),
+                hintText: 'Enter your code',
+                helperText: 'password',
+                prefix: Icon(Icons.password),
+                suffixIcon: IconButton(
+                    hoverColor: Colors.transparent,
+                    highlightColor: Colors.transparent,
+                    splashColor: Colors.transparent,
+                    onPressed: () {
+                      setState(() {
+                        b = !b;
+                      });
+                    },
+                    icon: Icon(b ? Icons.visibility_off : Icons.visibility)),
                 border: OutlineInputBorder()),
           ),
-          
         ],
       ),
     );
